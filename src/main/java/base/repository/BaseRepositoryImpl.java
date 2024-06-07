280032a7b7b1be8019e7c3351f8d2e2adf56bac3
mamad <you@example.com>
2024-06-07 06:23:32 +0330

    overRide findById method in BaseRepositoryImpl class

--- src/main/java/base/repository/BaseRepositoryImpl.java
+++ src/main/java/base/repository/BaseRepositoryImpl.java
@@ -4,6 +4,7 @@ import base.entity.BaseEntity;
 import lombok.AllArgsConstructor;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
 import java.io.Serializable;
 @AllArgsConstructor
@@ -19,6 +20,15 @@ public abstract class BaseRepositoryImpl<T extends BaseEntity<ID>, ID extends Se
         if (entity.getId() == null) session.persist(entity);
         else entity = session.merge(entity);
         return entity;
+    }
+
+    @Override
+    public T findById(ID id) {
+        Session session = sessionFactory.getCurrentSession();
+        Query<T> query =
+                session.createQuery(String.format("from %s where id =:id",getEntity()),getEntityClass());
+        query.setParameter("id",id);
+        return query.getSingleResultOrNull();
     }
 
 }
