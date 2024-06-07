7c5d4be94c136f11e9e7c3c53a928215eedc3276
mamad <you@example.com>
2024-06-07 07:38:48 +0330

    add static block to Validation class

--- src/main/java/utility/Validation.java
+++ src/main/java/utility/Validation.java
@@ -11,4 +11,13 @@ public class Validation {
     private static final Pattern TEXT;
     private static final Pattern DATE_STRING;
 
+    static {
+        PASSWORD = Pattern.compile("^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}$");
+        NAME = Pattern.compile("^[a-zA-Z ]{3,15}$");
+        EMAIL =Pattern.compile("^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
+        IMAGE = Pattern.compile(".*\\.(jpg|jpeg)$");
+        TEXT = Pattern.compile("[a-zA-Z0-9., ]{5,100}$");
+        DATE_STRING = Pattern.compile("^((?:19|20)[0-9][0-9])-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])$");
+    }
+
 }
