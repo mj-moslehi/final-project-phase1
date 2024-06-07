2b7655375ddf8735852c98525e04ea16663118d4
mamad <you@example.com>
2024-06-07 06:23:18 +0330

    overRide saveOrUpdate method in BaseRepositoryImpl class

--- src/main/java/base/repository/BaseRepositoryImpl.java
+++ src/main/java/base/repository/BaseRepositoryImpl.java
@@ -2,6 +2,7 @@ package base.repository;
 
 import base.entity.BaseEntity;
 import lombok.AllArgsConstructor;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 import java.io.Serializable;
@@ -11,5 +12,13 @@ public abstract class BaseRepositoryImpl<T extends BaseEntity<ID>, ID extends Se
         implements BaseRepository<T, ID>{
 
     protected SessionFactory sessionFactory;
+
+    @Override
+    public T saveOrUpdate(T entity) {
+        Session session = sessionFactory.getCurrentSession();
+        if (entity.getId() == null) session.persist(entity);
+        else entity = session.merge(entity);
+        return entity;
+    }
 
 }
