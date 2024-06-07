0764b3e7bf7e20fc42d839701c31eda67696faf6
mamad <you@example.com>
2024-06-07 06:20:10 +0330

    add findAll method signature BaseService interface

--- src/main/java/base/service/BaseService.java
+++ src/main/java/base/service/BaseService.java
@@ -3,6 +3,7 @@ package base.service;
 import base.entity.BaseEntity;
 
 import java.io.Serializable;
+import java.util.List;
 
 public interface BaseService <T extends BaseEntity<ID>, ID extends Serializable>{
 
@@ -11,4 +12,7 @@ public interface BaseService <T extends BaseEntity<ID>, ID extends Serializable>
     T findById(ID id);
 
     void delete(T t);
+
+    List<T> findAll ();
+
 }
