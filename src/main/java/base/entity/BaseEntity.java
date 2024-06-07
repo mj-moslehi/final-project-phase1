ddd3e360c70559b2a89c36b99d68d956135b05ca
mamad <you@example.com>
2024-06-07 06:09:43 +0330

    add BaseEntity class

---
+++ src/main/java/base/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package base.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@MappedSuperclass
+@SuperBuilder
+
+public class BaseEntity<ID extends Serializable> implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected ID id;
+}
