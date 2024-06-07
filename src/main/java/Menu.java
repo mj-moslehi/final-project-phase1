1e6f442109a6376ad594a2e891fd23a2f2664cd3
mamad <you@example.com>
2024-06-07 07:40:25 +0330

    add final fields to Menu class

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,2 +1,29 @@
+import service.admin.AdminService;
+import service.comment.CommentService;
+import service.customer.CustomerService;
+import service.expert.ExpertService;
+import service.expert_subService.Expert_SubServiceService;
+import service.orders.OrdersService;
+import service.service.ServiceService;
+import service.subService.SubServiceService;
+import service.suggestion.SuggestionService;
+import utility.ApplicationContext;
+
+import java.util.Scanner;
+
 public class Menu {
+
+    private final Scanner scanner = new Scanner(System.in);
+
+    private final AdminService adminService = ApplicationContext.getAdminService();
+    private final CommentService commentService = ApplicationContext.getCommentService();
+    private final CustomerService customerService = ApplicationContext.getCustomerService();
+    private final ExpertService expertService = ApplicationContext.getExpertService();
+    private final OrdersService ordersService = ApplicationContext.getOrdersService();
+    private final ServiceService serviceService = ApplicationContext.getServiceService();
+    private final SubServiceService subServiceService = ApplicationContext.getSubServiceService();
+    private final SuggestionService suggestionService = ApplicationContext.getSuggestionService();
+    private final Expert_SubServiceService expert_subServiceService = ApplicationContext.getExpertSubServiceService();
+
+
 }
