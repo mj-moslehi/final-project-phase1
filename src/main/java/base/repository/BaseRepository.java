92fa8d193cfd50f6d4abcbca8e1bb19d2217b116
mamad <you@example.com>
2024-06-07 06:12:20 +0330

    add saveOrUpdate signature to BaseRepository interface

--- src/main/java/base/repository/BaseRepository.java
+++ src/main/java/base/repository/BaseRepository.java
@@ -5,4 +5,7 @@ import base.entity.BaseEntity;
 import java.io.Serializable;
 
 public interface BaseRepository <T extends BaseEntity<ID>, ID extends Serializable>{
+
+    T saveOrUpdate(T entity);
+
 }
