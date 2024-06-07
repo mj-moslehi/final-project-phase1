8cd02bcec6fcca7cf6702dbf1d685623a122b58c
mamad <you@example.com>
2024-06-07 07:39:08 +0330

    add static methods to Validation class

--- src/main/java/utility/Validation.java
+++ src/main/java/utility/Validation.java
@@ -1,5 +1,6 @@
 package utility;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Validation {
@@ -18,6 +19,36 @@ public class Validation {
         IMAGE = Pattern.compile(".*\\.(jpg|jpeg)$");
         TEXT = Pattern.compile("[a-zA-Z0-9., ]{5,100}$");
         DATE_STRING = Pattern.compile("^((?:19|20)[0-9][0-9])-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])$");
+    }
+
+    public static boolean isValidPassword(String password) {
+        Matcher matcher = PASSWORD.matcher(password);
+        return matcher.find();
+    }
+
+    public static boolean isValidName(String name){
+        Matcher matcher = NAME.matcher(name);
+        return  matcher.find();
+    }
+
+    public static boolean isValidEmail(String email){
+        Matcher matcher = EMAIL.matcher(email);
+        return matcher.find();
+    }
+
+    public static boolean isValidImage(String image){
+        Matcher matcher = IMAGE.matcher(image);
+        return matcher.find();
+    }
+
+    public static boolean isValidText(String description){
+        Matcher matcher = TEXT.matcher(description);
+        return matcher.find();
+    }
+
+    public static boolean isValidDateString(String dateString){
+        Matcher matcher = DATE_STRING.matcher(dateString);
+        return matcher.find();
     }
 
 }
