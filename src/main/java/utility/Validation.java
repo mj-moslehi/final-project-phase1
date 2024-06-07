ce21d7300632213d3f5603aef8812eba37e8f056
mamad <you@example.com>
2024-06-07 07:38:31 +0330

    add fields to Validation class

--- src/main/java/utility/Validation.java
+++ src/main/java/utility/Validation.java
@@ -1,4 +1,14 @@
 package utility;
 
+import java.util.regex.Pattern;
+
 public class Validation {
+
+    private static final Pattern PASSWORD;
+    private static final Pattern NAME;
+    private static final Pattern EMAIL;
+    private static final Pattern IMAGE;
+    private static final Pattern TEXT;
+    private static final Pattern DATE_STRING;
+
 }
