f19519fe63f8d4596a148fa8ab61f27366c2baf8
mamad <you@example.com>
2024-06-07 06:26:19 +0330

    add fields to BaseServiceImpl class

--- src/main/java/base/service/BaseServiceImpl.java
+++ src/main/java/base/service/BaseServiceImpl.java
@@ -3,6 +3,7 @@ package base.service;
 import base.entity.BaseEntity;
 import base.repository.BaseRepository;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.SessionFactory;
 
 import java.io.Serializable;
 
@@ -11,5 +12,6 @@ import java.io.Serializable;
 public class BaseServiceImpl<T extends BaseEntity<ID>, ID extends Serializable,
         R extends BaseRepository<T, ID>>
         implements BaseService<T, ID> {
-
+    private final R repository;
+    private final SessionFactory sessionFactory;
 }
